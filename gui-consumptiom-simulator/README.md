2c51ef4645b3687347d537082db55710024cedb6
sidney-neto <bashrc.sid@gmail.com>
2019-06-22 23:23:35 -0400

    Add GUI Simulator

---
+++ gui-consumptiom-simulator/README.md
@@ -0,0 +1,23 @@
+# Beer Consumption Simulator
+GUI simulator developed by the Kivy framework in Python with the aim of predicting according to model, the average consumption of beer based on the features 'temp_max', 'chuva' and 'fds'.
+
+<p align="center">
+  <img src="https://im5.ezgif.com/tmp/ezgif-5-c9aed97a55ef.gif" width="400">
+</p>
+
+## Installing and running
+1. Clone repo
+```
+$ git clone https://github.com/sidney-neto/beer-consumption-analysis
+```
+
+2. Install dependencies
+```
+  $ cd beer-consumption-analysis/gui-consumptiom-simulator/
+  $ pip install -r requirements.txt
+```
+
+3. Run
+```
+$ python3 main.py
+```
---
+++ gui-consumptiom-simulator/main.py
@@ -0,0 +1,52 @@
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.properties import NumericProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+class BeerConsuptiom(Screen):
+    entrada = StringProperty()
+
+    def do_simulate(self, temp_max, chuva, fds):
+        app = App.get_running_app()
+        
+        app.temp_max = temp_max
+        app.chuva = chuva
+        app.fds = fds
+
+        df = pd.read_csv('../Datasets/Consumo_cerveja.csv', sep=';')
+        y = df['consumo']
+        X = df[['temp_max','chuva','fds']]
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2811)
+        modelo = LinearRegression()
+        modelo.fit(X_train,y_train)
+        y_previsto = modelo.predict(X_test)
+        
+        entrada = X_test[0:1]
+        entrada = [[float(temp_max), float(chuva), float(fds)]]
+        
+        self.entrada = str(modelo.predict(entrada)[0].round(2))
+        self.manager.get_screen('simulator').reset_form()
+    
+    def reset_form(self):
+        self.ids['temp_max'].text = ""
+        self.ids['chuva'].text = ""
+        self.ids['fds'].text = ""
+        
+class BeerConsuptiomSimulatorApp(App):
+    temp_max = StringProperty(None)
+    chuva = StringProperty(None)
+    fds = StringProperty(None)
+
+    def build(self):
+        manager = ScreenManager()
+        manager.add_widget(BeerConsuptiom(name='simulator'))
+        return manager
+
+if __name__ == '__main__':
+    BeerConsuptiomSimulatorApp().run()
